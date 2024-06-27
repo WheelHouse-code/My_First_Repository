19b6a57ab327f65ee257693dfe06ffbc4bd98d9c
WheelHouse-code <foxh3@qut.edu.au>
2024-06-28 08:25:48 +1000

    Update Learning to Create a Python Library.py

--- My_First_Package/Learning to Create a Python Library.py
+++ My_First_Package/Learning to Create a Python Library.py
@@ -1,1 +1,18 @@
 #Learning to Create a Python Library
+def add(x, y):
+    """Addition of two numbers."""
+    return x + y
+
+def subtract(x, y):
+    """Subtraction of two numbers."""
+    return x - y
+
+def multiply(x, y):
+    """Multiplication of two numbers."""
+    return x * y
+
+def divide(x, y):
+    """Division of two numbers."""
+    if y == 0:
+        raise ValueError("Cannot divide by zero!")
+    return x / y
