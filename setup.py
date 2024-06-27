8448ce04d7ca4de6097ff70ab60c60c9967f483e
WheelHouse-code <foxh3@qut.edu.au>
2024-06-27 17:51:01 +1000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Test_Library',
+    version='0.1',
+    packages=find_packages(),
+    description='To test the creation of a Python Library. ',
+    author='Haydn Andrew Fox',
+    license='MIT',
+)
